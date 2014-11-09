9c1c659f72c833565dc0645741ce3c04a6f10b6d
Daniel Ngo <danieln@inventivemw.com>
2014-11-09 23:50:59 +1100

    Added danielno user

---
+++ site/accounts/danielno.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: danielno
+email: hello@danielno.com
+password: >
+  $2a$10$guEFmIzi2JR7o21CZH3MdukkfejPRK99QKwfER9naq9/KLxPWeDSG
+language: en
+role: admin
+token: 538ad57fd2b09da8ff2794b814fac1c366f80657
+history:
+  - home
+  - about
+  - projects/wilson-joanna
+  - projects/caulfield-bier
+  - projects/roselia

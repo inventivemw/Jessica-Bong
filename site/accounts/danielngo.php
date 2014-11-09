aab7bb11bd827e5873341613b8e614d4780ff953
Daniel Ngo <danieln@inventivemw.com>
2014-11-09 23:47:38 +1100

    Unignored accounts

---
+++ site/accounts/danielngo.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: danielngo
+email: hello@danielno.com
+password: C4rc0s4r!
+language: en
+role: admin
+token: bd945449db5828ebde60b5a961c9c73cce3f2248
+history:
+  - projects/muma-exhibition-guide
+  - about
+  - projects

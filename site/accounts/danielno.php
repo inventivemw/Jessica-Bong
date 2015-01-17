5e3f728aecc86ca9a9ed6f1f75650bfb15c32e20
Daniel Ngo <danieln@inventivemw.com>
2015-01-17 19:47:02 +1100

    Updated panel user

--- site/accounts/danielno.php
+++ site/accounts/danielno.php
@@ -6,10 +6,10 @@ password: >
   $2a$10$guEFmIzi2JR7o21CZH3MdukkfejPRK99QKwfER9naq9/KLxPWeDSG
 language: en
 role: admin
-token: 538ad57fd2b09da8ff2794b814fac1c366f80657
+token: d67cde6f840c57608bf2dc516d2d30279d0f1c76
 history:
-  - home
+  - blog/the-imitation-game
+  - blog/the-first-cut-is-the-deepest
+  - blog/guardians-of-the-galaxy
   - about
-  - projects/wilson-joanna
-  - projects/caulfield-bier
-  - projects/roselia
+  - home
